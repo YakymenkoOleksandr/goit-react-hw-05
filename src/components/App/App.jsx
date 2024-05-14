6c72a4470084b4b8eb4c7512d4e0c3e743d17b4e
YakymenkoOleksandr <93155369+YakymenkoOleksandr@users.noreply.github.com>
2024-05-14 18:20:39 +0300

    addcomit2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,25 +3,25 @@ import HomePage from '../../pages/HomePage/HomePage';
 import './App.css';
 import axios from 'axios';
 
+const trendMouvies = 'https://api.themoviedb.org/3/trending/movie/day';
+const apiKey = {
+  headers: {
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YWE5MTAyOGM3M2YxNDUxMWU1ZDdhYzkwNjFkYmJkMyIsInN1YiI6IjY2NDMyYzg1YWI2MzYwNWZiNDc3ZTY2NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SFZIMdsLZSk8_fX_WuxlBKW65SVPPlnML-Y6QYZ0ApA',
+  },
+};
+
 export default function App() {
   const [results, setResults] = useState();
 
-  const trendMouvies = 'https://api.themoviedb.org/3/trending/movie/day';
-  const apiKey = {
-    headers: {
-      Authorization: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YWE5MTAyOGM3M2YxNDUxMWU1ZDdhYzkwNjFkYmJkMyIsInN1YiI6IjY2NDMyYzg1YWI2MzYwNWZiNDc3ZTY2NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SFZIMdsLZSk8_fX_WuxlBKW65SVPPlnML-Y6QYZ0ApA',
-    }
-  };
-
-    useEffect(() => {
-      function getTopMuvies() {
-       axios
-        .get(trendMouvies, { headers: apiKey.headers })
-        .then(response => setResults(response.data.results))
-        .catch(err => console.error(err));
-    }
-        getTopMuvies();
-        
+  useEffect(() => {
+    axios
+      .get(trendMouvies, apiKey)
+      .then(response => {
+        setResults(response.data.results);
+        console.log(response.data.results);
+      })
+      .catch(err => console.error(err));
   }, []);
 
   return (
