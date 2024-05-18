c57e0f7f4c6613ac0e360cbe660e29dd0cada357
YakymenkoOleksandr <93155369+YakymenkoOleksandr@users.noreply.github.com>
2024-05-18 15:57:22 +0300

    addcomit4

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,15 @@
-import { useEffect, useState } from 'react';
-import HomePage from '../../pages/HomePage/HomePage.jsx';
+import { useEffect, useState, lazy, Suspense } from 'react';
+/*import HomePage from '../../pages/HomePage/HomePage.jsx';*/
 import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage.jsx';
 import Navigation from '../Navigation/Navigation.jsx';
 import './App.css';
 import axios from 'axios';
 import { Routes, Route } from 'react-router-dom';
-import MoviesPage from '../../pages/MoviesPage/MoviesPage.jsx';
-import MovieDetailsPage from '../../pages/MovieDetailsPage/MovieDetailsPage.jsx';
-import MovieCast from '../MovieCast/MovieCast.jsx';
-import MovieReviews from '../MovieReviews/MovieReviews.jsx';
+/*import MoviesPage from '../../pages/MoviesPage/MoviesPage.jsx';*/
+/*import MovieDetailsPage from '../../pages/MovieDetailsPage/MovieDetailsPage.jsx';*/
+/*import MovieCast from '../MovieCast/MovieCast.jsx';*/
+/*import MovieReviews from '../MovieReviews/MovieReviews.jsx';*/
+
 const trendMouvies = 'https://api.themoviedb.org/3/trending/movie/day';
 
 const apiKey = {
@@ -17,6 +18,14 @@ const apiKey = {
       'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YWE5MTAyOGM3M2YxNDUxMWU1ZDdhYzkwNjFkYmJkMyIsInN1YiI6IjY2NDMyYzg1YWI2MzYwNWZiNDc3ZTY2NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SFZIMdsLZSk8_fX_WuxlBKW65SVPPlnML-Y6QYZ0ApA',
   },
 };
+
+const HomePage = lazy(() => import('../../pages/HomePage/HomePage.jsx'));
+const MoviesPage = lazy(() => import('../../pages/MoviesPage/MoviesPage.jsx'));
+const MovieDetailsPage = lazy(() =>
+  import('../../pages/MovieDetailsPage/MovieDetailsPage.jsx')
+);
+const MovieCast = lazy(() => import('../MovieCast/MovieCast.jsx'));
+const MovieReviews = lazy(() => import('../MovieReviews/MovieReviews.jsx'));
 
 export default function App() {
   const [results, setResults] = useState();
@@ -60,22 +69,24 @@ export default function App() {
     <>
       <Navigation />
       <Routes>
-        <Route path="/" element={<HomePage results={results} />} />
-        <Route
-          path="/movies"
-          element={
-            <MoviesPage
-              values={values}
-              movies={movies}
-              handleSubmit={handleSubmit}
-            />
-          }
-        />
-        <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-          <Route path="/movies/:movieId/cast" element={<MovieCast />} />
-          <Route path="/movies/:movieId/reviews" element={<MovieReviews />} />
-        </Route>
-        <Route path="*" element={<NotFoundPage />} />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Route path="/" element={<HomePage results={results} />} />
+          <Route
+            path="/movies"
+            element={
+              <MoviesPage
+                values={values}
+                movies={movies}
+                handleSubmit={handleSubmit}
+              />
+            }
+          />
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+  <Route path="cast" element={<MovieCast />} />
+  <Route path="reviews" element={<MovieReviews />} />
+</Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Suspense>
       </Routes>
     </>
   );
